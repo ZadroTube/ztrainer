271c9cae452f84cc70f6c29ddfb123f1a0a18353
ZadroTube <luxorxxx@gmail.com>
2026-05-15 14:50:46 +0800

    try to fix auth

--- src/components/auth/LoginScreen.tsx
+++ src/components/auth/LoginScreen.tsx
@@ -71,6 +71,7 @@ export function LoginScreen({ onAuthSuccess }: LoginScreenProps) {
       script.src = 'https://telegram.org/js/telegram-widget.js?22';
       script.setAttribute('data-telegram-login', 'ZadroTubikBot');
       script.setAttribute('data-size', 'large');
+      script.setAttribute('data-onauth', 'onTelegramAuth(user)');
       script.setAttribute('data-request-access', 'write');
       script.async = true;
 
